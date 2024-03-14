78e13021c5aa265a164bc691167ec1aec95151ac
Syahrieza <syahriezafauzi1@gmail.com>
2024-03-14 10:18:38 +0700

    dashboard slicing fixed

--- lib/modules/dashboard/view/ui/dashboard_view.dart
+++ lib/modules/dashboard/view/ui/dashboard_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:wiwikost/config/themes/colours.dart';
+import 'package:wiwikost/modules/dashboard/view/components/room_card.dart';
 
 class DashboardView extends StatelessWidget {
   const DashboardView({super.key});
@@ -9,11 +10,29 @@ class DashboardView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colours.antiFlashWhite,
-      body: Column(
+      body: Stack(
         children: [
+          SingleChildScrollView(
+            child: Column(
+              children: [
+                Container(
+                  margin: EdgeInsets.only(top: 60.w),
+                  width: 1.sw,
+                  height: 1.sh - (30.w + 60.w + 10.w * 3),
+                  child: ListView.separated(
+                      itemBuilder: (context, index) => RoomCard(),
+                      separatorBuilder: (context, index) => SizedBox(
+                            height: 10.w,
+                          ),
+                      itemCount: 3),
+                ),
+              ],
+            ),
+          ),
           Container(
             padding: EdgeInsets.all(10.w),
             width: 1.sw,
+            height: 60.w,
             color: Colours.white,
             child: Text(
               'Pilih kamarmu',
@@ -22,68 +41,6 @@ class DashboardView extends StatelessWidget {
                   ),
             ),
           ),
-          SizedBox(
-            height: 20.w,
-          ),
-          Container(
-            decoration: const BoxDecoration(
-              color: Colours.white,
-            ),
-            child: Column(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(20.0),
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(20.w),
-                    child: Image.network(
-                        'https://asset.olympicfurniture.co.id/NEWS/10-Desain-Kamar-Aesthetic-Minimalis-yang-Bikin-Betah.webp'),
-                  ),
-                ),
-                Row(
-                  children: [
-                    Container(
-                      padding: EdgeInsets.only(left: 20.w),
-                      alignment: Alignment.centerLeft,
-                      child: Text(
-                        "Kamar 1",
-                        style: Theme.of(context).textTheme.bodySmall,
-                      ),
-                    ),
-                    Spacer(),
-                    Container(
-                      padding: EdgeInsets.only(right: 20.w),
-                      alignment: Alignment.centerLeft,
-                      child: Text(
-                        "Lantai 1",
-                        style: Theme.of(context).textTheme.bodySmall,
-                      ),
-                    ),
-                  ],
-                ),
-                Container(
-                  padding: EdgeInsets.only(left: 20.w),
-                  alignment: Alignment.centerLeft,
-                  child: Text(
-                    "Tipe:  Kamar mandi dalam",
-                    style: Theme.of(context).textTheme.labelLarge,
-                  ),
-                ),
-                Container(
-                  padding: EdgeInsets.only(left: 20.w),
-                  alignment: Alignment.centerLeft,
-                  child: Text(
-                    "Rp.1.000.000 - Rp.1.500.000",
-                    style: Theme.of(context).textTheme.labelLarge!.copyWith(
-                          color: Colours.greenPrimary1,
-                        ),
-                  ),
-                ),
-                SizedBox(
-                  height: 20.w,
-                ),
-              ],
-            ),
-          )
         ],
       ),
     );
